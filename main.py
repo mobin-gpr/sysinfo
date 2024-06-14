ac0e615b7b7877a9b59d5efb7af57f3002636389
mobin-gpr <mohammad.mobin.ghanbarpour79@gmail.com>
2024-06-15 01:36:05 +0330

    clean code by black

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ from rich.table import Table
 
 console = Console()
 
+
 # Function to get OS information
 def get_os_info():
     table = Table(title="Operating System Information")
     table.add_column("System", justify="center", style="cyan", no_wrap=True)
     table.add_column("OS Release", justify="center", style="cyan")
     table.add_column("OS Version", justify="center", style="magenta")
-    
+
     table.add_row(
         platform.system(),
         str(platform.release()),
         str(platform.version()),
     )
-    
+
     return table
+
 
 # Function to get CPU information
 def get_cpu_info():
@@ -29,7 +31,7 @@ def get_cpu_info():
     table.add_column("Core count", justify="start", style="cyan")
     table.add_column("Physical cores", justify="start", style="magenta")
     table.add_column("CPU Usage (%)", justify="start", style="green")
-    
+
     usage_percent = psutil.cpu_percent(interval=1, percpu=False)
     if usage_percent <= 25:
         usage_str = f"[bold green]{usage_percent}%[/bold green]"
@@ -39,44 +41,46 @@ def get_cpu_info():
         usage_str = f"[bold yellow]{usage_percent}%[/bold yellow]"
     else:
         usage_str = f"[bold red]{usage_percent}%[/bold red]"
-        
+
     table.add_row(
         platform.processor(),
         str(psutil.cpu_count(logical=True)),
         str(psutil.cpu_count(logical=False)),
-        str(usage_str)
+        str(usage_str),
     )
-    
+
     return table
+
 
 # Function to get RAM information
 def get_ram_info():
     svmem = psutil.virtual_memory()
-    
+
     table = Table(title="RAM Information")
     table.add_column("Total memory (GB)", justify="center", style="cyan", no_wrap=True)
     table.add_column("Available memory (GB)", justify="center", style="cyan")
     table.add_column("Used memory (%)", justify="center", style="magenta")
-    
+
     table.add_row(
         str(round(svmem.total / (1024**3), 2)),
         str(round(svmem.available / (1024**3), 2)),
-        str(svmem.percent)
+        str(svmem.percent),
     )
-    
+
     return table
+
 
 # Function to get Disk information
 def get_disk_info():
     partitions = psutil.disk_partitions()
-    
+
     table = Table(title="Disk Information")
     table.add_column("Device", justify="start", style="cyan", no_wrap=True)
     table.add_column("Total size (GB)", justify="start", style="cyan")
     table.add_column("Used size (GB)", justify="start", style="magenta")
     table.add_column("Free size (GB)", justify="start", style="green")
     table.add_column("Usage (%)", justify="start")
-    
+
     for partition in partitions:
         try:
             partition_usage = psutil.disk_usage(partition.mountpoint)
@@ -85,7 +89,7 @@ def get_disk_info():
             used = round(partition_usage.used / (1024**3), 2)
             free = round(partition_usage.free / (1024**3), 2)
             usage_percent = partition_usage.percent
-            
+
             if usage_percent <= 25:
                 usage_str = f"[bold green]{usage_percent}%[/bold green]"
             elif usage_percent <= 50:
@@ -94,18 +98,13 @@ def get_disk_info():
                 usage_str = f"[bold yellow]{usage_percent}%[/bold yellow]"
             else:
                 usage_str = f"[bold red]{usage_percent}%[/bold red]"
-            
-            table.add_row(
-                device,
-                str(total),
-                str(used),
-                str(free),
-                usage_str
-            )
+
+            table.add_row(device, str(total), str(used), str(free), usage_str)
         except PermissionError:
             continue
-        
+
     return table
+
 
 # Function to get GPU information
 def get_gpu_info():
@@ -114,7 +113,7 @@ def get_gpu_info():
     table.add_column("GPU", justify="start", style="cyan", no_wrap=True)
     table.add_column("Memory Total (GB)", justify="start", style="cyan")
     table.add_column("Memory Used (GB)", justify="start", style="magenta")
-        
+
     for gpu in gpu_info:
         usage_percent = gpu.memoryUsed
         if usage_percent <= 25:
@@ -125,29 +124,27 @@ def get_gpu_info():
             usage_str = f"[bold yellow]{usage_percent}%[/bold yellow]"
         else:
             usage_str = f"[bold red]{usage_percent}%[/bold red]"
-            
-        table.add_row(
-            gpu.name,
-            str(gpu.memoryTotal),
-            str(usage_str)
-        )
-    
+
+        table.add_row(gpu.name, str(gpu.memoryTotal), str(usage_str))
+
     return table
+
 
 # Function to get Network interface information
 def get_network_info():
     network_info = {}
-    
+
     table = Table(title="Network Interface Information")
     table.add_column("Interface", justify="start", style="cyan", no_wrap=True)
     table.add_column("IP Address", justify="start", style="cyan")
-    
+
     for interface, addresses in psutil.net_if_addrs().items():
         ip_address = ", ".join([address.address for address in addresses])
         network_info[interface] = ip_address
         table.add_row(interface, ip_address)
-        
+
     return table
+
 
 # Add command line argument parser
 parser = argparse.ArgumentParser(description="Display system information")
@@ -157,9 +154,12 @@ parser.add_argument("--cpu", action="store_true", help="Display CPU information"
 parser.add_argument("--ram", action="store_true", help="Display RAM information")
 parser.add_argument("--disk", action="store_true", help="Display Disk information")
 parser.add_argument("--gpu", action="store_true", help="Display GPU information")
-parser.add_argument("--network", action="store_true", help="Display Network interface information")
+parser.add_argument(
+    "--network", action="store_true", help="Display Network interface information"
+)
 
 args = parser.parse_args()
+
 
 # Function to display system information based on user input
 def display_system_info(args):
@@ -170,7 +170,7 @@ def display_system_info(args):
         console.print("\n", get_disk_info())
         console.print("\n", get_gpu_info())
         console.print("\n", get_network_info())
-        
+
     if args.os:
         console.print("\n", get_os_info())
     if args.cpu:
@@ -183,6 +183,7 @@ def display_system_info(args):
         console.print("\n", get_gpu_info())
     if args.network:
         console.print("\n", get_network_info())
+
 
 if __name__ == "__main__":
     display_system_info(args)
